a0c29918c2ef3a03777827f8508c33a7b7151b04
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-04 12:11:17 +0700

    Update setCaption.py

--- caption/setCaption.py
+++ caption/setCaption.py
@@ -21,19 +21,19 @@ def setCaption(update,context):
   if update.message.reply_to_message is not None:
    fileCaption= update.message.text
    fileType=update.message.reply_to_message
-   if fileType.document!=None:
+   if fileType.document!=None:p
      update.message.reply_document(
        update.message.reply_to_message.document.file_id,caption=fileCaption)
-   elif fileType.video!=None:
+   elif fileType.video!=None:o
      update.message.reply_video(
        update.message.reply_to_message.video.file_id,caption=fileCaption)
-   elif fileType.audio!=None:
+   elif fileType.audio!=None:d
      update.message.reply_audio(
        update.message.reply_to_message.audio.file_id,caption=fileCaption)
-   elif fileType.voice!=None:
+   elif fileType.voice!=None:k
      update.message.reply_voice(
        update.message.reply_to_message.voice.file_id,caption=fileCaption)
-   elif fileType.photo!=None:
+   elif fileType.photo!=None:b
      update.message.reply_photo(update.message.reply_to_message.photo[-1].file_id,caption=fileCaption)
   else:
     update.message.reply_text(update.message.text)
