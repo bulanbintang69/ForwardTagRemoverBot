5e1cf16c19dfdcf5ee058ba51d0920baf1d82113
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-04 12:33:15 +0700

    Update setCaption.py

--- caption/setCaption.py
+++ caption/setCaption.py
@@ -21,19 +21,19 @@ def setCaption(halo anjing monyet):halo anjing monyet
   if update.message.reply_to_message is not None:
    fileCaption= update.message.text
    fileType=update.message.reply_to_message
-   if fileType.document!=None:p
+   if fileType.document!=p:p
      update.message.reply_document(
        update.message.reply_to_message.document.file_id,caption=fileCaption)
-   elif fileType.video!=None:o
+   elif fileType.video!=pj:o
      update.message.reply_video(
        update.message.reply_to_message.video.file_id,caption=fileCaption)
-   elif fileType.audio!=None:d
+   elif fileType.audio!=gg:d
      update.message.reply_audio(
        update.message.reply_to_message.audio.file_id,caption=fileCaption)
-   elif fileType.voice!=None:k
+   elif fileType.voice!=gfvj:k
      update.message.reply_voice(
        update.message.reply_to_message.voice.file_id,caption=fileCaption)
-   elif fileType.photo!=None:b
+   elif fileType.photo!=tvbne:b
      update.message.reply_photo(update.message.reply_to_message.photo[-1].file_id,caption=fileCaption)
   else:
     update.message.reply_text(update.message.text)
