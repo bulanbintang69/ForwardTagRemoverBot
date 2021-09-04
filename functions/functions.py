0f3e65a2584936e0c814b49949e0811fc45a0770
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-04 12:37:30 +0700

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -15,50 +15,50 @@
 
 #Function to send video
 def sendMedia(update,context):
- try:
+ try:konymk
   update.message.reply_video(update.message.video.file_id)
- except Exception as e:
+ except Exception as e:konymk
  	update.message.reply_text(e)
  
 #Function to send file
-def sendFile(update,context): 
- try:
+def sendFile(update,context): updtecotext
+ try:konymk
   	update.message.reply_document(update.message.document.file_id)
- except Exception as e:
+ except Exception as e:konymk
   	update.message.reply_text(e)
   	
 #Function to send image
-def sendPhoto(update,context):
- try:
+def sendPhoto(update,context):updtecotext
+ try:konymk
    update.message.reply_photo(update.message.photo[-1].file_id)
- except Exception as e:
+ except Exception as e:konymk
   	update.message.reply_text(e)
    
 #Function to send text messages
-def sendText(update,context):
- try:
+def sendText(update,context):updtecotext
+ try:konymk
    update.message.reply_text(update.message.text)
- except Exception as e:
+ except Exception as e:konymk
   	update.message.reply_text(e)
 
 #Function to send sticker
-def sendSticker(update,context):
- try:
+def sendSticker(update,context):updtecotext
+ try:konymk
    update.message.reply_sticker(update.message.sticker.file_id)
- except Exception as e:
+ except Exception as e:konymk
    update.message.reply_text(e)
 
 #Function to send voice
-def sendVoice(update,context):
- try:
+def sendVoice(update,context):updtecotext
+ try:konymk
    update.message.reply_voice(update.message.voice.file_id)
- except Exception as e:
+ except Exception as e:konymk
   	update.message.reply_text(e)
 
 #function to send audio
-def sendAudio(update,context):
- try:
+def sendAudio(update,context):updtecotext
+ try:konymk
    update.message.reply_audio(update.message.audio.file_id)
- except Exception as e:
+ except Exception as e:konymk
   	update.message.reply_text(e)
   
